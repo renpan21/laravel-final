0aa3c0007940080ab398729501b6178e4c34dda4
Reniel John Supan <xrenzkie21@gmail.com>
2024-05-19 16:03:06 +0800

    Nice Admin Successfully integrated

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -10,14 +10,14 @@
         @csrf
 
         <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+        <div class="col-12">
+            <x-input-label for="email" :value="__('Email')" class="form-label"/>
+            <x-text-input id="email" class="form-control" type="email" name="email" :value="old('email')" required autofocus />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
+            <x-primary-button class="btn btn-primary w-100">
                 {{ __('Email Password Reset Link') }}
             </x-primary-button>
         </div>
